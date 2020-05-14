24aabc1b2292857cd01e60d409b46aff19865139
cyphergremory <41551157+cyphergremory@users.noreply.github.com>
2020-05-14 05:14:32 -0500

    Update empleados.sql

--- empleados.sql
+++ empleados.sql
@@ -1,32 +1,7 @@
--- phpMyAdmin SQL Dump
--- version 4.8.5
--- https://www.phpmyadmin.net/
---
--- Host: localhost:3306
--- Generation Time: May 14, 2020 at 10:12 AM
--- Server version: 5.7.24
--- PHP Version: 7.2.19
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `crud`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `empleados`
---
 
 CREATE TABLE `empleados` (
   `Id` int(11) NOT NULL,
@@ -36,34 +11,10 @@ CREATE TABLE `empleados` (
   `Salary` decimal(10,0) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `empleados`
---
-
-INSERT INTO `empleados` (`Id`, `Name`, `Surname`, `Position`, `Salary`) VALUES
-(10, 'Jorge Alberto', 'Davila Perez', 'Anestesiologo', '10000');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `empleados`
---
 ALTER TABLE `empleados`
   ADD PRIMARY KEY (`Id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `empleados`
---
 ALTER TABLE `empleados`
   MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
